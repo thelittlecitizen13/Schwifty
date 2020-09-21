e0c34cfb4fbfb0d486f4e94b143eaa72c154ba44
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-09-21 20:33:11 +0300

    table contains random numbers according to game size

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,64 @@
 function createTable()
 {
+    generateTable();
+}
+
+function generateTable()
+{
     var gameSize = document.getElementById("gameSize").value;
     var tableDiv = document.getElementById("GameTable");
+    var gameNumbers = gameSize*gameSize - 1;
+    var randomNumbersArray = generateRandomNumbersArray(gameNumbers)
     tableDiv.innerHTML = "";
     var tbl = document.createElement('table');
-  tbl.style.width = '60%';
-  tbl.style.height = '60%';
-  tbl.setAttribute('border', '1');
-  var tbdy = document.createElement('tbody');
-  for (var i = 0; i < gameSize; i++) {
-    var tr = document.createElement('tr');
-    for (var j = 0; j < gameSize; j++) {
-        var td = document.createElement('td');
-        //td.appendChild(document.createTextNode('\u0020'))
-        //i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-        tr.appendChild(td)
-      }
-      tbdy.appendChild(tr);
-    }
+    tbl.style.width = '60%';
+    tbl.style.height = '60%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < gameSize; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < gameSize; j++) {
+            var td = document.createElement('td');
+            
+            if (i != gameSize-1 || j != gameSize-1)
+            {
+                var num = randomNumbersArray.pop();
+                td.innerHTML = num;
+            }
+            tr.appendChild(td)
+        }
+        tbdy.appendChild(tr);
+        }
+        
     
-  
-  tbl.appendChild(tbdy);
-  tableDiv.appendChild(tbl);
+    tbl.appendChild(tbdy);
+    tableDiv.appendChild(tbl);
 }
+
+function generateRandomNumbersArray(arraySize)
+{
+    var numbersArray = [...Array(arraySize + 1).keys()]
+    numbersArray.shift();
+    numbersArray = shuffle(numbersArray);
+    return numbersArray;
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
